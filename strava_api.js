f0b63cd07e5bb567ad162473a6ccb2381e37f8b6
sidneyamsalem <sidneyamsalem@gmail.com>
2024-06-16 17:16:10 -0400

    bug fix

--- strava_api.js
+++ strava_api.js
@@ -4,7 +4,7 @@
 function getActivities(){
 
     // accesstoken = "0fa0ae202f7092f5dfb5adf76b7bf7acb0cb932c"
-    const activites_link = "https://www.strava.com/api/v3/athlete/activities?access_token=0fa0ae202f7092f5dfb5adf76b7bf7acb0cb932c"
+    const activities_link = "https://www.strava.com/api/v3/athlete/activities?access_token=0fa0ae202f7092f5dfb5adf76b7bf7acb0cb932c"
 
     fetch(activities_link)
         .then((res) => console.log(res.json()))
