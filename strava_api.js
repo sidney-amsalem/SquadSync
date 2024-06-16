2cf7bc1300c23911af46585431d70244233785c9
sidneyamsalem <sidneyamsalem@gmail.com>
2024-06-16 14:39:20 -0400

    added getActivities function, which fetches user activity data via access token

---
+++ strava_api.js
@@ -0,0 +1,13 @@
+// Link to access athlete token
+// https://www.strava.com/api/v3/athlete/activities?access_token=0fa0ae202f7092f5dfb5adf76b7bf7acb0cb932c
+
+function getActivities(){
+
+    accesstoken = "0fa0ae202f7092f5dfb5adf76b7bf7acb0cb932c"
+    const activites_link = "https://www.strava.com/api/v3/athlete/activities?access_token=" + accesstoken
+
+    fetch(activities_link)
+        .then((res) => console.log(res.json()))
+}
+
+getActivities()
